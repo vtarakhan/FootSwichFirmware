4b98118a02ceae508ff9982626326fa1d5066840
vtarakhan <v.tarakhan@kasta.ua>
2026-06-10 10:30:43 +0300

    fix py

--- generate_eeprom.py
+++ generate_eeprom.py
@@ -1,30 +1,31 @@
 import os
-Import subprocess
-Import env
+import subprocess
 
-Def make_eeprom(source, target, env):
+def make_eeprom(source, target, env):
     # Отримуємо шляхи до файлу прошивки .elf та майбутнього .eep
-    Elf_file = os.path.join(env.subst("$BUILD_DIR"), "firmware.elf")
-    Eep_file = os.path.join(env.subst("$BUILD_DIR"), "firmware.eep")
+    elf_file = os.path.join(env.subst("$BUILD_DIR"), "firmware.elf")
+    eep_file = os.path.join(env.subst("$BUILD_DIR"), "firmware.eep")
     
     # Визначаємо шлях до інструменту avr-objcopy з пакета gcc-avr
-    Objcopy = env.subst("$OBJCOPY")
+    objcopy = env.subst("$OBJCOPY")
     
-    Print(f"--> Extracting EEPROM data to: {eep_file}")
+    print(f"--> Extracting EEPROM data to: {eep_file}")
     
     # Запускаємо команду копіювання секції .eeprom
-    Cmd = [
-        Objcopy,
+    cmd = [
+        objcopy,
         "-j", ".eeprom",
         "--set-section-flags=.eeprom=alloc,load",
         "--change-section-lma", ".eeprom=0",
         "--no-change-warnings",
         "-O", "ihex",
-        Elf_file,
-        Eep_file
+        elf_file,
+        eep_file
     ]
     
-    Subprocess.run(cmd)
+    subprocess.run(cmd)
 
 # Реєструємо функцію для виконання ПІСЛЯ того, як збудується головний .elf файл
+# Використовуємо глобальну змінну 'env', яку PlatformIO автоматично прокидає в скрипт
+global env
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", make_eeprom)
